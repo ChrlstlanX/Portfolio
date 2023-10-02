cac189d745b4f0822ea1f233790273ecc9089877
Cristian Bautista Pérez <crhlstl4nk09@gmail.com>
2023-10-02 11:41:34 -0600

    Portfolio finalizado

---
+++ index.js
@@ -0,0 +1,5 @@
+import "./assets/javascript/fixed.js";
+import "./assets/javascript/main.js";
+// import "node_modules/eins-modal/dist/js/eins-modal-plain.min.js";
+
+import "./assets/css/index.css";
